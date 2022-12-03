759ece0a40c8e585809096b423685488985384a0
Constace <vuongnhat777@gmail.com>
2022-12-03 16:23:51 +0700

    Added Solution.

---
+++ Snak3 Classic/Snak3 Classic/snak3.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.locals import *
+
+import assets.framework as frwk
+
+# Initialize Pygame.
+pygame.init()
+
+# FPS clock.
+mainClock = pygame.time.Clock()
+framerate = 60
+
+monitor_size = [pygame.display.Info().current_w, pygame.display.Info().current_h]
+
+# Create windows.
+pygame.display.set_caption('Snak3: Classic')
+pygame.display.set_icon(pygame.image.load('assets/images/snak3.png'))
+screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
+ws = [screen.get_width(), screen.get_height()]  # Window Size.
+
+# Create instance.
+main_sys = frwk.Main_System(screen, ws, monitor_size, mainClock, framerate)
+
+# Gọi main menu để chạy cả chương trình khi và chỉ khi snak3.py là chương trình chính.
+if __name__ == '__main__':
+	main_sys.main_menu()
